df550bef0e383dd46f4edfdcc8bda8c5f55b997d
Martin Klimcik <martin.klimcik@gmail.com>
2021-03-16 21:00:53 +0100

    feat: login page elements

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -1,8 +1,26 @@
 import { Component } from "react";
+import Dropdown from "../Dropdown";
+import Button from "../Button";
+
+const testusers = [
+  { label: "Mates", value: "mates" },
+  { label: "Tester", value: "tester" },
+];
 
 class Login extends Component {
   render() {
-    return <div>Login View</div>;
+    return (
+      <div>
+        <div>
+          <h1>Login</h1>
+        </div>
+        <form>
+          <Dropdown options={testusers} />
+          <Button text="Add new" />
+          <Button text="Confirm" />
+        </form>
+      </div>
+    );
   }
 }
 
