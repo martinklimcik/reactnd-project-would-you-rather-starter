d851e03620d4fdc0c339ab409e0e877423b1895a
Martin Klimcik <martin.klimcik@gmail.com>
2021-03-17 15:11:26 +0100

    feat: create new poll elements

--- src/components/views/New.js
+++ src/components/views/New.js
@@ -1,9 +1,24 @@
 import { Component } from "react";
+import Button from "../Button";
+import Input from "../Input";
 import "./views.css";
 
 class New extends Component {
   render() {
-    return <div>New View</div>;
+    return (
+      <div>
+        <h1>Create New Poll</h1>
+        <p>Would You Rather</p>
+        <form>
+          <Input placeholder="Answer A" />
+          <p>OR</p>
+          <Input placeholder="Answer B" />
+          <div>
+            <Button text="Submit" />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
