ce54b17587e266fa90021964f2916239ba77ec60
Martin Klimcik <martin.klimcik@gmail.com>
2021-03-21 18:03:35 +0100

    feat: add question functionality

--- src/components/views/New.js
+++ src/components/views/New.js
@@ -1,21 +1,54 @@
 import { Component } from "react";
 import { connect } from "react-redux";
-import Button from "../Button";
-import Input from "../Input";
+import { handleAddQuestion } from "../../actions/questions";
 import "./views.css";
 
 class New extends Component {
+  state = { optionOne: "", optionTwo: "", confirmation: false };
+
+  handleOptionOne = (e) => {
+    this.setState({ optionOne: e.target.value });
+  };
+  handleOptionTwo = (e) => {
+    this.setState({ optionTwo: e.target.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.group("New.handleSubmit");
+    console.log(this.state.optionOne);
+    console.log(this.state.optionTwo);
+    console.groupEnd();
+    const { optionOne, optionTwo } = this.state;
+    const { dispatch } = this.props;
+    dispatch(handleAddQuestion(optionOne, optionTwo));
+    this.setState({ optionOne: "", optionTwo: "", confirmation: true });
+  };
+
+  // TODO: submit button disabled css
+  // TODO: state.confirmation to show confirmation of question successfully added and button to go to voting screen for it
   render() {
+    const submitDisabled =
+      this.state.optionOne === "" && this.state.optionTwo === "";
     return (
       <div className="view">
         <h1>Create New Poll</h1>
         <p className="wyr">Would You Rather</p>
-        <form>
-          <Input placeholder="First option" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="First option"
+            className="input"
+            onChange={this.handleOptionOne}
+          ></input>
           <p className="wyr">OR</p>
-          <Input placeholder="Second option" />
+          <input
+            placeholder="Second option"
+            className="input"
+            onChange={this.handleOptionTwo}
+          ></input>
           <div>
-            <Button text="Submit" cls="submit" />
+            <button type="submit" className="submit" disabled={submitDisabled}>
+              Submit
+            </button>
           </div>
         </form>
       </div>
